daa4bf79df11df758b3db0f4b6e82b8170abe0b3
mohamedeld <mohamed.azoz20010@gmail.com>
2026-07-08 14:21:59 +0300

    add button click

--- src/Button.tsx
+++ src/Button.tsx
@@ -3,7 +3,7 @@ import GoogleAnalytics from "./analytics/GoogleAnalytics";
 export const Button = () => {
   const handleClick = () => {
     console.log("click");
-    GoogleAnalytics.trackEvent("button_click", {
+    GoogleAnalytics.trackEvent("button_clickss", {
       category: "engagement",
       label: "cta_hero",
     });
